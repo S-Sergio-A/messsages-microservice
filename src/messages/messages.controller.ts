c0fcb307ced3b53a2a0d38bae61b56d6102000b9
Sergey <sergios33033@gmail.com>
2021-07-12 16:11:36 +0300

    All HTTP API endpoints removed, one microservice endpoint added.

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,38 +1,14 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-// import { MessagePattern } from "@nestjs/microservices";
-// import { MessagesGateway } from "./messages.gateway";
-// import { ApiCreatedResponse, ApiOperation } from "@nestjs/swagger";
+import { MessagePattern, Payload, RpcException, Transport } from "@nestjs/microservices";
+import { Controller } from "@nestjs/common";
+import { MessageDocument } from "./message.schema";
+import { MessagesService } from "./messages.service";
 
 @Controller("messages")
 export class MessagesController {
-  // constructor(private readonly messagesGateway: MessagesGateway) {}
+  constructor(private readonly messagesService: MessagesService) {}
 
-  // @Post()
-  // @ApiOperation({ summary: "Send the entrance's message." })
-  // @ApiCreatedResponse({})
-  // @MessagePattern({ cmd: "send-message" })
-  // async sendMessage(@Body() createUserDto: { message: "BOOBA" }) {
-  //   this.messagesGateway.server.emit("send-message", "Hello from REST API");
-  // }
-  //
-  // @Post()
-  // @ApiOperation({ summary: "Delete the entrance's message depending on a message id." })
-  // @ApiCreatedResponse({})
-  // @MessagePattern({ cmd: "delete-message" })
-  // async deleteMessage() {
-  //   this.messagesGateway.server.emit("delete-message", "Hello from REST API");
-  // }
-  //
-  // @Post()
-  // @ApiOperation({ summary: "Send the entrance's message with an attachment." })
-  // @ApiCreatedResponse({})
-  // @MessagePattern({ cmd: "send-message-with-attachment" })
-  // async sendMessageWithAttachment() {
-  //   this.messagesGateway.server.emit("send-message-with-attachment", "Hello from REST API");
-  // }
-
-  @Get()
-  async booba() {
-    return { message: "Hello" };
+  @MessagePattern({ cmd: "search-message" }, Transport.REDIS)
+  async sendMessage(@Payload() keyword: string): Promise<MessageDocument[] | RpcException> {
+    return this.messagesService.searchMessages(keyword);
   }
 }
