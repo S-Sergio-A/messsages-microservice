1e5a87cb3ae6288472502ebf1fa88ea75989b8f0
Sergey <sergios33033@gmail.com>
2021-07-12 16:07:58 +0300

    Swagger removed, all origins were allowed for development purposes.

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,22 @@
+import { WsAdapter } from "@nestjs/platform-ws";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { WsAdapter } from "@nestjs/platform-ws";
 import helmet from "helmet";
+import "reflect-metadata";
 
 async function bootstrap() {
-  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-  //   transport: Transport.TCP,
-  //   options: {
-  //     host: "0.0.0.0",
-  //     port: 9000
-  //   }
-  // });
-
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
   app.enableCors({
-    origin: [process.env.FRONT_URL],
+    origin: [process.env.FRONT_URL, "*"],
     credentials: true,
     exposedHeaders: ["Access-Token", "Refresh-Token", "Client-Token", "Country", "Content-Type"],
     methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"]
   });
 
-  // const config = new DocumentBuilder()
-  //   .setTitle("Authentication and entrance microservice")
-  //   .setDescription("The cats API description")
-  //   .setVersion("1.0")
-  //   .addTag("authentication, authorization, entrance")
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup("api", app, document);
-
   app.useWebSocketAdapter(new WsAdapter(app));
-  // app.listen(() => console.log("Microservice is listening"));
-  await app.listen(7000);
+  await app.listen(process.env.PORT || 7000);
 }
 
 bootstrap();
