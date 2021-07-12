6a464625c3c3c536918225bb524cde03d73581ae
Sergey <sergios33033@gmail.com>
2021-07-12 16:07:00 +0300

    The ThrottlerGuard added and configured.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 import { MongooseModule } from "@nestjs/mongoose";
 import { ConfigModule } from "@nestjs/config";
+import { APP_GUARD } from "@nestjs/core";
 import { Module } from "@nestjs/common";
 import { ValidationModule } from "./pipes/validation.module";
 import { MessageModule } from "./messages/messages.module";
@@ -7,11 +9,22 @@ import { MessageModule } from "./messages/messages.module";
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ThrottlerModule.forRoot({
+      ttl: 180,
+      limit: 1200,
+      ignoreUserAgents: [new RegExp("googlebot", "gi"), new RegExp("bingbot", "gi")]
+    }),
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URL}/${process.env.MONGO_DATABASE_NAME}?retryWrites=true&w=majority`
     ),
     MessageModule,
     ValidationModule
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
   ]
 })
 export class AppModule {}
