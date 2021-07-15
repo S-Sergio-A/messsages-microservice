dbf57f3c969b596d6f5ba3c5c5819f4f98d3744f
Sergey <sergios33033@gmail.com>
2021-07-15 22:37:45 +0300

    Minor fixes.

--- src/pipes/validation/message.validation.pipe.ts
+++ src/pipes/validation/message.validation.pipe.ts
@@ -9,10 +9,6 @@ export class MessageValidationPipe implements PipeTransform {
       throw new BadRequestException("No data submitted");
     }
 
-    if (!metadata.metatype || !this._toValidate(metadata.metatype)) {
-      return value;
-    }
-
     const { errors, isValid } = await ValidationService.prototype.validateMessage(JSON.parse(value));
 
     if (isValid) {
@@ -20,9 +16,5 @@ export class MessageValidationPipe implements PipeTransform {
     } else {
       throw new ValidationException(errors);
     }
-  }
-
-  private _toValidate(metatype): boolean {
-    return typeof metatype === "object";
   }
 }
