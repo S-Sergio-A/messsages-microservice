172e6193e9b6fb8383a5ca9431d461266af02b27
Sergey <sergios33033@gmail.com>
2021-07-25 14:07:20 +0300

    Types fixed.

--- src/messages/schemas/message.schema.ts
+++ src/messages/schemas/message.schema.ts
@@ -1,13 +1,14 @@
 import { Schema, SchemaFactory, Prop } from "@nestjs/mongoose";
 import { Document, Types } from "mongoose";
 import { UserDocument } from "./user.schema";
+import { RoomDocument } from "./room.schema";
 
 export type MessageDocument = Message & Document;
 
 @Schema()
 class Message {
-  @Prop({ required: true, index: false, ref: "Room" })
-  roomId: string;
+  @Prop({ required: true, index: false, ref: "Room", type: Types.ObjectId })
+  roomId: RoomDocument | Types.ObjectId;
 
   @Prop({ required: true, index: false, ref: "User", type: Types.ObjectId })
   user: UserDocument | Types.ObjectId;
